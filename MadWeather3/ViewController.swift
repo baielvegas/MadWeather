8be50f027136c7f737f94c58c71609ea3a01144e
baielvegas <30592102+baielvegas@users.noreply.github.com>
2019-05-07 00:08:19 +0600

    Initial Commit

---
+++ MadWeather3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MadWeather3
+//
+//  Created by Isaev Baiel on 5/7/19.
+//  Copyright © 2019 MadDevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
